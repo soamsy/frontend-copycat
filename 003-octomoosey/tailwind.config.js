ab618f788e0c2e54445adf0c3708cbfbc9201a1f
Soamsy <thsoams@gmail.com>
2023-05-08 06:08:54 -0600

    003 Add navigation

--- 003-octomoosey/tailwind.config.js
+++ 003-octomoosey/tailwind.config.js
@@ -10,6 +10,19 @@ module.exports = {
       }
     },
   },
-  plugins: ["flowbite/plugin"],
+  plugins: ["flowbite/plugin", function({ addUtilities, addComponents, e, prefix, config }) {
+    const newUtilities = {
+      '.horizontal-tb': {
+        writingMode: 'horizontal-tb',
+      },
+      '.vertical-rl': {
+        writingMode: 'vertical-rl'
+      },
+      '.vertical-lr': {
+        writingMode: 'vertical-lr'
+      }
+    }
+    addUtilities(newUtilities)
+  }],
 }
 
