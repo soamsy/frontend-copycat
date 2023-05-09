bc79aae2d81254f2ab6893e1a0ce7ad4e007c83d
Soamsy <thsoams@gmail.com>
2023-05-09 07:27:56 -0600

    004 Create initial layout with carousel

---
+++ 004-yuzame-label/src/App.vue
@@ -0,0 +1,42 @@
+<script >
+  import Carousel from './components/Carousel.vue'
+  
+  export default {
+    components: {
+      Carousel
+    }
+  }
+</script>
+
+<template>
+  <div class="min-h-screen h-full w-full bg-[url('./assets/bg.jpg')]">
+    <div class="container mx-auto">
+      <nav class="py-2 flex justify-center items-end font-bold tracking-tighter">
+        <a href="#" class="pt-40 px-10 hover:bg-[url('./assets/home.png')] bg-no-repeat bg-[0_-167px] hover:bg-center transition-[background-position] duration-500 text-black hover:text-opacity-50">
+          home
+        </a>
+        <a href="#" class="pt-40 px-10 hover:bg-[url('./assets/news.png')] bg-no-repeat bg-[0_-167px] hover:bg-center transition-[background-position] duration-500 text-black hover:text-opacity-50">
+          news
+        </a>
+        <a href="#" class="pt-40 px-10 hover:bg-[url('./assets/about.png')] bg-no-repeat bg-[0_-167px] hover:bg-center transition-[background-position] duration-500 text-black hover:text-opacity-50">
+          about
+        </a>
+        <a class="mx-10 hover:opacity-50" href="#"><img class="mx-10" src="./assets/g-nav-home-logo.png"></a>
+        <a href="#" class="pt-40 px-10 hover:bg-[url('./assets/music.png')] bg-no-repeat bg-[0_-167px] hover:bg-center transition-[background-position] duration-500 text-black hover:text-opacity-50">
+          music
+        </a>
+        <a href="#" class="pt-40 px-10 hover:bg-[url('./assets/shop.png')] bg-no-repeat bg-[0_-167px] hover:bg-center transition-[background-position] duration-500 text-black hover:text-opacity-50">
+          shop
+        </a>
+        <a href="#" class="pt-40 px-10 hover:bg-[url('./assets/contact.png')] bg-no-repeat bg-[0_-167px] hover:bg-center transition-[background-position] duration-500 text-black hover:text-opacity-50">
+          contact
+        </a>
+      </nav>
+      <div class="flex justify-center">
+        <Carousel></Carousel>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ 004-yuzame-label/src/components/Carousel.vue
@@ -0,0 +1,89 @@
+<script>
+    export default {
+        data() {
+            return {
+                slides: [
+                    "src/assets/RBMF2018.png",
+                    "src/assets/meister.png",
+                    "src/assets/yuzame-join-tokyosento.png",
+                    "src/assets/akairo.png",
+                    "src/assets/iwanttotellyou_top.png",
+                    "src/assets/plusa_top.png"
+                ],
+                current: 0,
+                transitioning: false,
+                interval: null
+            }
+        },
+        mounted: function () {
+            this.replaceInterval();
+        },
+        methods: {
+            replaceInterval: function () {
+                if (this.interval)
+                    clearInterval(this.interval);
+                this.interval = setInterval(() => {
+                    this.updateCurrent(1);
+                }, 6500);
+            },
+            updateCurrent: function(add) {
+                this.transitioning = false;
+                this.replaceInterval();
+                setTimeout(() => {
+                    this.current = (this.current + add) % this.slides.length;
+                    this.transitioning = true;
+                }, 0);
+            },
+            slideStyle: function(i) {
+                let shifted = i - this.current + 1
+                let place = this.mod(shifted, this.slides.length) - 1;
+                let zIndex = 0;
+                switch (place) {
+                    case 0:
+                        zIndex = 5;
+                        break;
+                    case 1:
+                        zIndex = 4;
+                        break;
+                    case -1:
+                        zIndex = 3;
+                        break;
+                    case 2:
+                        zIndex = 2;
+                        break;
+                    case -2:
+                        zIndex = 1;
+                        break;
+                }
+                if (!this.transitioning) {
+                    return `z-index: ${zIndex};` +
+                            `transform: translateX(${(place) % this.slides.length * 100}%);`
+                }
+
+                return `transform: translateX(${(place) % this.slides.length * 100}%);` +
+                        `z-index: ${zIndex};` +
+                        `transition: transform 1600ms ease-in-out;`
+                        ;
+            },
+            mod: function(n, m) {
+                return ((n % m) + m) % m;
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="flex flex-col items-center max-w-[960px] w-full h-[280px] relative overflow-hidden">
+        <div v-for="(slide, index) in slides" class="absolute" :style="slideStyle(index)">
+            <img class="" :src="slide" alt="">
+        </div>
+        <a class="absolute top-0 bottom-0 left-0 pl-2 flex flex-col justify-center z-10">
+            <button @click="updateCurrent(-1)" class="w-8 h-8 rounded-full text-white bg-gray-800"><i class="fa fa-chevron-left fa-xs"></i></button>
+        </a>
+        <a class="absolute top-0 bottom-0 right-0 pr-2 flex flex-col justify-center z-10">
+            <button @click="updateCurrent(1)" class="w-8 h-8 rounded-full text-white bg-gray-800"><i class="fa fa-chevron-right fa-xs"></i></button>
+        </a>
+    </div>
+</template>
+
+<style scoped></style>
