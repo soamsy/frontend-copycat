31e70ba4379b513447ec3b8528ac5e94c6442c24
Soamsy <thsoams@gmail.com>
2023-05-11 01:19:04 -0600

    005 Create logic for kana guessing game

--- 005-learn-kana/src/App.vue
+++ 005-learn-kana/src/App.vue
@@ -1,94 +1,33 @@
 <script setup>
   import KanaTable from './components/KanaTable.vue'
-import { ref, reactive } from 'vue';
-
-const kana = ref('あ');
-const showOptions = ref(true);
-const input = ref('');
-const successes = ref(0);
-const total = ref(0);
-const error = ref('');
+import { ref, reactive, watch, onMounted, onUpdated } from 'vue';
+import tables from './tables.js';
 
 let hiragana = reactive({
   toggles: new Set(),
-  display: [
-  [["あ", "a"], ["い", "i"], ["う", "u"], ["え", "e"], ["お", "o"]],
-  [["か", "ka"], ["き", "ki"], ["く", "ku"], ["け", "ke"], ["こ", "ko"]],
-  [["さ", "sa"], ["し", "shi"], ["す", "su"], ["せ", "se"], ["そ", "so"]],
-  [["た", "ta"], ["ち", "chi"], ["つ", "tsu"], ["て", "te"], ["と", "to"]],
-  [["な", "na"], ["に", "ni"], ["ぬ", "nu"], ["ね", "ne"], ["の", "no"]],
-  [["は", "ha"], ["ひ", "hi"], ["ふ", "fu"], ["へ", "he"], ["ほ", "ho"]],
-  [["ま", "ma"], ["み", "mi"], ["む", "mu"], ["め", "me"], ["も", "mo"]],
-  [["や", "ya"], ["", ""], ["ゆ", "yu"], ["", ""], ["よ", "yo"]],
-  [["ら", "ra"], ["り", "ri"], ["る", "ru"], ["れ", "re"], ["ろ", "ro"]],
-  [["わ", "wa"], ["", ""], ["", ""], ["", ""], ["を", "wo"]],
-  [["", ""], ["", ""], ["", ""], ["", ""], ["ん", "n"]],
-  [["が", "ga"], ["ぎ", "gi"], ["ぐ", "gu"], ["げ", "ge"], ["ご", "go"]],
-  [["ざ", "za"], ["じ", "ji"], ["ず", "zu"], ["ぜ", "ze"], ["ぞ", "zo"]],
-  [["だ", "da"], ["ぢ", "ji"], ["づ", "zu"], ["で", "de"], ["ど", "do"]],
-  [["ば", "ba"], ["び", "bi"], ["ぶ", "bu"], ["べ", "be"], ["ぼ", "bo"]],
-  [["ぱ", "pa"], ["ぴ", "pi"], ["ぷ", "pu"], ["ぺ", "pe"], ["ぽ", "po"]],
-  ]
+  display: tables.hiragana
 })
 
 let hiraganaCombinations = reactive({
   toggles: new Set(),
-  display: [
-    [["きゃ", "kya"], ["きゅ", "kyu"], ["きょ", "kyo"], ],
-    [["しゃ", "sha"], ["しゅ", "shu"], ["しょ", "sho"], ],
-    [["ちゃ", "cha"], ["ちゅ", "chu"], ["ちょ", "cho"], ],
-    [["にゃ", "nya"], ["にゅ", "nyu"], ["にょ", "nyo"], ],
-    [["ひゃ", "hya"], ["ひゅ", "hyu"], ["ひょ", "hyo"], ],
-    [["みゃ", "mya"], ["みゅ", "myu"], ["みょ", "myo"], ],
-    [["りゃ", "rya"], ["りゅ", "ryu"], ["りょ", "ryo"], ],
-    [["ぎゃ", "gya"], ["ぎゅ", "gyu"], ["ぎょ", "gyo"], ],
-    [["じゃ", "ja"], ["じゅ", "ju"], ["じょ", "jo"], ],
-    [["ぢゃ", "ja"], ["ぢゅ", "ju"], ["ぢょ", "jo"], ],
-    [["びゃ", "bya"], ["びゅ", "byu"], ["びょ", "byo"], ],
-    [["ぴゃ", "pya"], ["ぴゅ", "pyu"], ["ぴょ", "pyo"], ],
-  ]
+  display: tables.hiraganaCombinations
 })
 
-// アイウエオ
 let katakana = reactive({
   toggles: new Set(),
-  display: [
-    [["ア", "a"], ["イ", "i"], ["ウ", "u"], ["エ", "e"], ["オ", "o"], ],
-    [["カ", "ka"], ["キ", "ki"], ["ク", "ku"], ["ケ", "ke"], ["コ", "ko"], ],
-    [["サ", "sa"], ["シ", "shi"], ["ス", "su"], ["セ", "se"], ["ソ", "so"], ],
-    [["タ", "ta"], ["チ", "chi"], ["ツ", "tsu"], ["テ", "te"], ["ト", "to"], ],
-    [["ナ", "na"], ["ニ", "ni"], ["ヌ", "nu"], ["ネ", "ne"], ["ノ", "no"], ],
-    [["ハ", "ha"], ["ヒ", "hi"], ["フ", "fu"], ["ヘ", "he"], ["ホ", "ho"], ],
-    [["マ", "ma"], ["ミ", "mi"], ["ム", "mu"], ["メ", "me"], ["モ", "mo"], ],
-    [["ヤ", "ya"], ["", ""], ["ユ", "yu"], ["", ""], ["ヨ", "yo"], ],
-    [["ラ", "ra"], ["リ", "ri"], ["ル", "ru"], ["レ", "re"], ["ロ", "ro"], ],
-    [["ワ", "wa"], ["", ""], ["", ""], ["", ""], ["ヲ", "wo"], ],
-    [["", ""], ["", ""], ["", ""], ["", ""], ["ン", "n"], ],
-    [["ガ", "ga"], ["ギ", "gi"], ["グ", "gu"], ["ゲ", "ge"], ["ゴ", "go"], ],
-    [["ザ", "za"], ["ジ", "ji"], ["ズ", "zu"], ["ゼ", "ze"], ["ゾ", "zo"], ],
-    [["ダ", "da"], ["ヂ", "ji"], ["ヅ", "zu"], ["デ", "de"], ["ド", "do"], ],
-    [["バ", "ba"], ["ビ", "bi"], ["ブ", "bu"], ["ベ", "be"], ["ボ", "bo"], ],
-    [["パ", "pa"], ["ピ", "pi"], ["プ", "pu"], ["ペ", "pe"], ["ポ", "po"], ],
-  ]
+  display: tables.katakana
 })
 
 let katakanaCombinations = reactive({
   toggles: new Set(),
-  display: [
-    [["キャ", "kya"], ["キュ", "kyu"], ["キョ", "kyo"], ],
-    [["シャ", "sha"], ["シュ", "shu"], ["ショ", "sho"], ],
-    [["チャ", "cha"], ["チュ", "chu"], ["チョ", "cho"], ],
-    [["ニャ", "nya"], ["ニュ", "nyu"], ["ニョ", "nyo"], ],
-    [["ヒャ", "hya"], ["ヒュ", "hyu"], ["ヒョ", "hyo"], ],
-    [["ミャ", "mya"], ["ミュ", "myu"], ["ミョ", "myo"], ],
-    [["リャ", "rya"], ["リュ", "ryu"], ["リョ", "ryo"], ],
-    [["ギャ", "gya"], ["ギュ", "gyu"], ["ギョ", "gyo"], ],
-    [["ジャ", "ja"], ["ジュ", "ju"], ["ジョ", "jo"], ],
-    [["ヂャ", "ja"], ["ヂュ", "ju"], ["ヂョ", "jo"], ],
-    [["ビャ", "bya"], ["ビュ", "byu"], ["ビョ", "byo"], ],
-    [["ピャ", "pya"], ["ピュ", "pyu"], ["ピョ", "pyo"], ],
-  ],
+  display: tables.katakanaCombinations,
 });
+
+const showOptions = ref(true);
+const input = ref('');
+const successes = ref(0);
+const error = ref('');
+const history = reactive([]);
 
 const updateTable = (table, index, value) => {
   if (value) 
@@ -97,29 +36,95 @@ const updateTable = (table, index, value) => {
     table.toggles.delete(index);
 }
 
+const getNext = () => {
+  let count = hiragana.toggles.size + hiraganaCombinations.toggles.size + katakana.toggles.size + katakanaCombinations.toggles.size;
+  console.log(count);
+  if (count === 0) {
+    hiragana.toggles.add(0);
+  }
+
+  let lastAnswered = history?.[history.length - 1]?.[0];
+  let options = [];
+  for (let { toggles, display } of [hiragana, hiraganaCombinations, katakana, katakanaCombinations]) {
+    for (let index of toggles) {
+      options.push.apply(options, display[index].filter(([kana, _]) => kana));
+    }
+  }
+
+  if (options.length === 1) {
+    return options[0];
+  }
+
+  options = options.filter(([kana, _]) => kana !== lastAnswered);
+
+  let index = Math.floor(Math.random() * options.length);
+  return options[index];
+}
+
+const selected = ref(getNext());
+
+watch(input, () => {
+  if (!input.value)
+    return;
+  const [kana, romaji] = selected.value;
+  if (input.value === romaji) {
+    history.push(selected.value);
+    selected.value = getNext();
+    if (!error.value) {
+      successes.value++;
+    }
+    input.value = '';
+    error.value = '';
+  } else if (romaji.startsWith(input.value)) {
+    return;
+  } else {
+    error.value = `${kana} = ${romaji}`;
+  }
+})
+
+
 </script>
 
 <template>
   <div class="w-full min-h-screen bg-zinc-900 text-white">
+    <a id="top"></a>
+    <a href="#top" class="fixed p-5 right-3 bottom-6 rounded bg-neutral-700 hover:bg-neutral-100 hover:text-black">▲</a>
     <div class="py-4 px-2 container mx-auto lg:max-w-3xl">
-      <h1 class="text-3xl">Learn Kana</h1>
+      <h1 class="text-3xl">Learn kana</h1>
       <hr class="my-6 border-dashed">
       <div class="m-4 p-6 shadow-xl flex flex-col items-center">
-        <div class="text-9xl text-center">{{ kana }}</div>
+        <div class="text-9xl text-center">{{ selected?.[0] }}</div>
         <input v-model="input" class="my-6 w-72 bg-zinc-800 text-center" type="text">
-        <div v-if="!error && total == 0" class="text-lg">Hover over the kana to show its romanization and type the answer.</div>
+        <div class="min-h-[2rem]">
+          <div v-if="!error && history.length == 0" class="text-lg">Hover over the kana to show its romanization and type the answer.</div>
+          <div v-if="error" class="text-red-500 text-2xl">{{ error }}</div>
+        </div>
         <div class="my-2 self-start flex gap-x-4">
           <button class="py-2 px-3 shadow-md underline-button">Play sound</button>
           <button class="py-2 px-3 shadow-md underline-button">Stroke order</button>
         </div>
+        <div v-if="history.length > 0" class="mt-6 self-start"> {{ successes }} / {{ history.length }}</div>
       </div>
       <hr class="my-6 border-dashed">
       <button @click="showOptions = !showOptions" class="py-2 px-6 block mx-auto font-semibold shadow-lg underline-button">show/hide options</button>
-      <div v-if="showOptions" class="my-8">
+      <div v-if="showOptions" class="my-8 flex flex-col gap-y-2">
         <KanaTable title="Hiragana" :toggles="hiragana.toggles" @update:toggle="(i, value) => updateTable(hiragana, i, value)" :display="hiragana.display"></KanaTable>
         <KanaTable title="Hiragana combinations" :toggles="hiraganaCombinations.toggles" @update:toggle="(i, value) => updateTable(hiraganaCombinations, i, value)" :display="hiraganaCombinations.display"></KanaTable>
         <KanaTable title="Katakana" :toggles="katakana.toggles" @update:toggle="(i, value) => updateTable(katakana, i, value)" :display="katakana.display"></KanaTable>
         <KanaTable title="Katakana combinations" :toggles="katakanaCombinations.toggles" @update:toggle="(i, value) => updateTable(katakanaCombinations, i, value)" :display="katakanaCombinations.display"></KanaTable>
+        <h2 class="my-4 text-xl">Explanation</h2>
+        <p class="my-2 text-lg">Grind a row of kana until you stop making mistakes, then add another row. Cross reference the <code class="p-1 font-mono text-sm bg-black">kana audio chart</code> link to make sure you are pronouncing the kana correctly. If you wish to write, the <code class="p-1 font-mono text-sm bg-black">Stroke order</code> link will show you the proper form. You can save a few keystrokes on DJT Kana and your IME with the shortcuts below.</p>
+        <img class="w-1/2 mx-auto" src="./assets/shortcuts.png" alt="">
+        <p class="my-2 text-lg">The two dots (<a class="link" target="_blank" href="https://en.wikipedia.org/wiki/Dakuten_and_handakuten">dakuten</a>) next to certain kana indicate that the consonant is "muddied". For example, か (ka) becomes が (ga). The consonants transform as follows:</p>
+        <ul class="pl-10 text-lg list-disc">
+          <li>k -> g</li>
+          <li>t -> d</li>
+          <li>h/f -> b</li>
+          <li>s/ts -> z</li>
+          <li>sh/ch -> j</li>
+        </ul>
+        <p class="my-2 text-lg">A few kana add a circle to indicate a "p" transformation. For example, ほ (ho) becomes ぽ (po).</p>
+        <p class="my-2 text-lg">The smaller kana ゅ, ょ, and ゃ are used to combine kana sounds. For example, in ぎゃ the consonant of ぎ (gi) combines with the sound from や (ya): gya. The small っ (distinct from つ/tsu) is not itself pronounced, but lengthens the consonant that follows, as in にっぽん (<a class="link" target="_blank" href="https://upload.wikimedia.org/wikipedia/commons/d/db/Ja-nippon%28%E6%97%A5%E6%9C%AC%29.ogg">nippon</a>); the effect of holding your mouth in the consonant's position resembles a small pause. Tae Kim has more information.</p>
       </div>
     </div>
   </div>
